2a778a5bff715d72d3527524d059faf7ee94efed
Tarsis Lima <br.tmvdl@gmail.com>
2025-12-23 11:06:56 -0300

    Update PyGame TicTacToe example's files

--- pygame-tictactoe.py
+++ pygame-tictactoe.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-import time
 import COLORS
 
 pygame.init()
@@ -10,11 +9,10 @@ height = 600
 weight = 15
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Tic Tac Toe")
+pygame.display.set_caption('Tic Tac Toe')
 
-table = [[None] * 3] * 3
-print(f'Table: {table}')
-cur_player = "x"
+table = [[None, None, None], [None, None, None], [None, None, None]]
+cur_player = 'x'
 font_size = 100
 font = pygame.font.SysFont(None, font_size)
 game_over = False
@@ -29,53 +27,57 @@ def draw_table():
 def draw_symbols():
     for line in range(3):
         for column in range(3):
-            if table[line][column] is not None:
+            if table[line][column] != None:
                 text = font.render(table[line][column], True, COLORS.BLUE_1)
-                screen.blit(text, (column * (width//3) + 25, line * (height//3) + 10 ))
+                screen.blit(text, (column * (width//3) + (width//4), line * (height//3) + (height//30)))
 
 def check_winner():
-    # horizontal
-    for line in table:
-        if line.count(line[0]) == 3 and line[0] is not None:
-            return line[0]
-    # vertical
+    for line in range(3):
+        if table[line][0] != None and table[line][0] == table[line][1] == table[line][2]:
+            return table[line][0]
     for col in range(3):
-        if table[0][col] == table[1][col] == table[2][col] and table[0][col] is not None:
+        if table[0][col] != None and table[0][col] == table[1][col] == table[2][col]:
             return table[0][col]
-    # diagonal
-    if table[0][0] == table[1][1] == table[2][2] and table[0][0] is not None:
+    if table[0][0] != None and table[0][0] == table[1][1] == table[2][2]:
         return table[0][0]
-    if table[0][2] == table[1][1] == table[2][0] and table[0][2] is not None:
+    if table[0][2] != None and table[0][2] == table[1][1] == table[2][0]:
         return table[0][2]
     return None
 
-while game_over is not True:
-    draw_table()
-    draw_symbols()
+def play(line, column):
+    global game_over, cur_player
 
+    if table[line][column] is None:
+        table[line][column] = cur_player
+        winner = check_winner()
+
+        if winner != None:
+            print('Player', winner, 'wins!')
+            game_over = True
+
+        cur_player = 'o' if cur_player == 'x' else 'x'
+
+def handle_quit():
+    pygame.quit()
+    sys.exit()
+
+def handle_mouse_click():
+    x, y = pygame.mouse.get_pos()
+    line = y // (height // 3)
+    column = x // (width // 3)
+    play(line, column)
+
+def handle_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            handle_quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            column = x // (width // 3)
-            line = y // (height // 3)
+            handle_mouse_click()
 
-            print(f'x: {x}, y: {y}; column: {column}, line: {line}')
-
-            if table[line][column] is None:
-                table[line][column] = cur_player
-                winner = check_winner()
-
-                print(f'winner: {winner}')
-                
-                if winner is not None:
-                    print("Player", winner, "wins!")
-                    game_over = True
-
-                cur_player = "o" if cur_player == "x" else "x"
-                
+while game_over != True:
+    draw_table()
+    draw_symbols()
+    handle_events()
     pygame.display.update()
 
 pygame.quit()
