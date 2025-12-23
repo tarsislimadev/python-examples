ebac9526bbda9e09287c8c72046f38dd36785706
Tarsis Lima <br.tmvdl@gmail.com>
2025-12-23 12:28:09 -0300

    Update PyAutoGUI Mouse Positions example's files

--- pyautogui-mouse-positions.py
+++ pyautogui-mouse-positions.py
@@ -1,5 +1,13 @@
-import pyautogui
+import pyautogui, time
 
-for _ in range(20):
+SLEEP = 10
+
+cur_time = lambda: int(time.time())
+
+start_time = cur_time()
+end_time = start_time + SLEEP
+print(f'start: {start_time}; end: {end_time}')
+
+while cur_time() < end_time:
   x, y = pyautogui.position()
-  print(f'x: {x}, y: {y}')
+  print(f'x: {x}; y: {y}')
--- pyautogui-mouse-sleep.py
+++
@@ -1,9 +0,0 @@
-import pyautogui, time
-
-events = []
-
-for _ in range(200):
-  events.append(pyautogui.position())
-  time.sleep(0.1)
-
-print(f'{len(events)} mouse positions')
