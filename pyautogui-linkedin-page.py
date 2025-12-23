2a47a1abac58efa64334292fb4d41b61618e75c5
Tarsis Lima <br.tmvdl@gmail.com>
2025-12-23 12:27:34 -0300

    Add PyAutoGUI Linkedin Page example's files

---
+++ pyautogui-linkedin-page.py
@@ -0,0 +1,5 @@
+import pyautogui as pg
+
+pg.hotkey('win', '1') # opens brave at postion 1 in windows bar
+pg.click(None, 75) # clicks on navigation bar
+pg.typewrite('linkedin.com\n') # types 'linkedin.com'
