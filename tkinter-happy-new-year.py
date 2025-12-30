42c68b9e1e8245c12d2070a3ebac8a66966d9de4
Tarsis Lima <br.tmvdl@gmail.com>
2025-12-30 09:33:38 -0300

    Add Tkinter Happy New Year example's files

---
+++ tkinter-happy-new-year.py
@@ -0,0 +1,9 @@
+# https://www.instagram.com/p/DSwBOz4lB9_/
+
+import tkinter
+
+tk = tkinter.Tk()
+tk.configure(bg="black")
+tkinter.Label(tk, text="Happy New Year 2026", fg="gold", bg="black", font=("Arial", 22, "bold")).pack()
+tkinter.Label(tk, text="Wishing you joy!", fg="white", bg="black").pack()
+tk.mainloop()
