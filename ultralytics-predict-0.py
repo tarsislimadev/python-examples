532486fe03b9f1192ebac1c1b5323f260def5e1f
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-01-23 09:26:56 -0300

    Update ultralytics example's files

--- ultralytics-predict-0.py
+++ ultralytics-predict-0.py
@@ -1,3 +1,7 @@
+# https://docs.ultralytics.com/modes/predict/#key-features-of-predict-mode
+
+# python -m pip install opencv-python ultralytics
+
 from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt")
