c25344772c7b39b7e242cb9507d8540e1b79ac26
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-01-29 14:47:03 +0000

    Add Flask with Graphene example's files

---
+++ flask-graphene.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+import graphene
+
+class Query(graphene.ObjectType):
+  hello = graphene.String(name=graphene.String(default_value="World"))
+
+  def resolve_hello(self, info, name):
+    return f'Hello {name}!'
+
+schema = graphene.Schema(query=Query)
+
+app = Flask(__name__)
+
+app.add_url_rule(
+  '/graphql',
+  view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
